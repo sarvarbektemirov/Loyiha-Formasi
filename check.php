4a9e66793d1fe899a801ff14e7ded5037d1d8d6b
Sarvar <sarvarbektemirov422@gmail.com>
2026-02-04 09:12:30 +0500

    forma qo'shildi

---
+++ check.php
@@ -0,0 +1,9 @@
+<?php
+$a = htmlspecialchars($_POST['a'], ENT_QUOTES);
+$b = htmlspecialchars($_POST['b'], ENT_QUOTES);
+$c = htmlspecialchars($_POST['c'], ENT_QUOTES);
+$d = htmlspecialchars($_POST['d'], ENT_QUOTES);
+$e = htmlspecialchars($_POST['e'], ENT_QUOTES);
+
+echo " Familiaysi: " . $a . "<br> Ismi " . $b . " <br>Jinsi: " . $c . "<br> " . $d . "<br> " . $e;
+?>
---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loyiha fayllarini yuklash</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+
+<body class="container">
+    <h1 class="text-center">
+        Loyiha fayllarini yuklash
+    </h1>
+
+    <form action="check.php" method="POST" enctype="multipart/form-data">
+        <div class="row">
+
+            <div class="col-4 my-3 fw-bold">
+                <label for="a" class="form-label">Loyiha Nomi:</label>
+                <input type="text" name="a" id="a" class="form-control">
+            </div>
+
+            <div class="col-4 my-3 fw-bold">
+                <label for="b" class="form-label">Loyiha Fayli:</label>
+                <input type="file" name="b" id="b" class="form-control">
+            </div>
+
+            <div class="col-4 my-3 fw-bold">
+                <label for="c" class="form-label">Byudjet:</label>
+                <input type="text" name="c" id="c" class="form-control">
+            </div>
+
+            <div class="col-4 my-3 fw-bold">
+                <label for="d" class="form-label">Tugallanish Sanasi:</label>
+                <input type="date" name="d" id="d" class="form-control">
+            </div>
+
+            <div class="col-8 my-3 fw-bold">
+                <label for="e" class="form-label">Tavsif:</label>
+                <textarea name="e" id="e" class="form-control" rows="3"></textarea>
+            </div>
+
+            <div class="col-12 my-4 text-center">
+                <button type="submit" class="btn btn-primary px-5">Yuborish</button>
+            </div>
+
+        </div>
+    </form>
+
+</body>
+
+</html>
